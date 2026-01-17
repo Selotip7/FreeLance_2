ba95ff4df8498a244d8467e07404ef67b14118a1
Selotip7 <selotip7@gmail.com>
2026-01-17 14:02:06 +0700

    test return total pengajuan

---
+++ app/Http/Controllers/Api/PengaduanController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Pengaduan;
+
+class PengaduanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        // $total = Pengaduan::count(); // hitung semua baris
+        // return response()->json([
+        //     'total_pengaduan' => $total
+        // ]);
+    }
+
+    public function totalPengaduan()
+    {
+        $total = Pengaduan::count(); // hitung semua baris
+        return response()->json([
+            'total_pengaduan' => $total
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
