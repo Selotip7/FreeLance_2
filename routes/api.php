744f11f125dd9fb3e594c0c5a65629aea28259c8
Selotip7 <selotip7@gmail.com>
2026-01-17 15:18:30 +0700

    test return total pengajuan

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
+
 use App\Http\Controllers\Api\PengaduanController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('pengaduan', [PengaduanController::class, 'index']);
-Route::get('pengaduan', [PengaduanController::class, 'totalPengaduan']);// semua pengaduan
-Route::get('pengaduan/{id}', [PengaduanController::class, 'show']); // detail pengaduan
-Route::get('my-pengaduan', [PengaduanController::class, 'myReports']); // pengaduan user login
-Route::get('pengaduan/status/{status}', [PengaduanController::class, 'status']); // filter status
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-?>
+
+Route::get('/pengaduan', [PengaduanController::class, 'totalPengaduan']);
