8ac5872eebf60b77fabf66d983ba7fe937b73cab
Selotip7 <selotip7@gmail.com>
2026-01-17 14:03:00 +0700

    test return total pengajuan

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\Api\PengaduanController;
 
-Route::get('pengaduan', [PengaduanController::class, 'index']);
+// Route::get('pengaduan', [PengaduanController::class, 'index']);
 Route::get('pengaduan', [PengaduanController::class, 'totalPengaduan']);// semua pengaduan
 Route::get('pengaduan/{id}', [PengaduanController::class, 'show']); // detail pengaduan
 Route::get('my-pengaduan', [PengaduanController::class, 'myReports']); // pengaduan user login
